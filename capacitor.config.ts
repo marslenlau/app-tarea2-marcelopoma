8b90941c44493bc21dcecaa35883c6d2a5e83622
Marcelo Poma <marcelo.pc.dev@gmail.com>
2024-03-19 19:55:53 -0400

    ADD: tarea numero 2

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tarea1marcelo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
